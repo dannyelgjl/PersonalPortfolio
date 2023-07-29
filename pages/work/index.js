7f572e01531ee3d258266233ad67ae2867b74d9b
dannyelgjl <dannyelgjl@gmail.com>
2023-07-29 19:52:49 -0300

    feat: added scroll

--- pages/work/index.js
+++ pages/work/index.js
@@ -9,7 +9,7 @@ import { fadeIn } from "../../variants";
 
 const Work = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
+    <div className="md:overflow-visible overflow-auto h-full bg-primary/30 py-36 flex items-center">
       <Circles />
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">

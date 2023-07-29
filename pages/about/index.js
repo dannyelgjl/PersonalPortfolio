0e19b59589025b14e7d76f063c5652abb349dc4c
dannyelgjl <dannyelgjl@gmail.com>
2023-07-29 19:54:55 -0300

    feat: added scroll in about tab

--- pages/about/index.js
+++ pages/about/index.js
@@ -156,7 +156,7 @@ const About = () => {
   const [index, setIndex] = useState(0);
   console.log(index);
   return (
-    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+    <div className="md:overflow-visible overflow-auto h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
       {/* avatar img */}
       <motion.div

d9ecf31907869e74e9d62eac63328262ace8fd48
dannyelgjl <dannyelgjl@gmail.com>
2023-07-29 20:19:48 -0300

    feat: added zindex

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       {/* text */}
-      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+      <div className="z-10 w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="z-10 text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
